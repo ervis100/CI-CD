ed6158bda20889263e5c0f739a234ab977d3cea1
ervis <ervislusha100@gmail.com>
2023-10-19 09:49:17 +0100

    first commit

---
+++ src/main/java/com/example/demo/controllers/CICDController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class CICDController {
+
+    @Value("${response.welcome}")
+    private String welcome;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return welcome;
+    }
+
+}
